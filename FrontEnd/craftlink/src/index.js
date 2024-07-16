fbe5f9cfe0ad31002eec86a6548f6bdfb6f686d8
riri-bygit <rethabileb13@gmail.com>
2024-07-16 20:51:24 +0200

    Index.js

--- FrontEnd/craftlink/src/index.js
+++ FrontEnd/craftlink/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import { BrowserRouter, createBrowserRouter,RouterProvider } from "react-router-dom";
 import Homepage from './Components/HomePage/homePage'
 import ServiceForm from './Components/Services/ServiceForm'
 import Navbar from './Components/NavBar/NavBar';
 import Layout from './Components/Layout';
 
 const router = createBrowserRouter([{
+  path: "/landingpage",
+  element: <Landing/>,
+
   path:'/',
   element: <Layout/>,
   children:[
@@ -29,7 +32,9 @@ element: <ServiceForm/>,
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    
     {/* <App /> */}
+    
     <RouterProvider router={router}/>
 
   </React.StrictMode>
