879dcf98799a35ed36d1e3b6f1c1b833a7da566a
riri-bygit <rethabileb13@gmail.com>
2024-07-16 20:55:54 +0200

    index.js

---
+++ BackEnd/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const serviceRoutes = require('./routes/serviceRoutes');
+const connectDB = require("./dbcon");
+require("dotenv").config();
+
+const app = express();
+const port = 5000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// MongoDB connection
+connectDB();
+
+// Routes
+app.use('/api/service', serviceRoutes);
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
