256e4405c102c9180e8a583f43693c2a8f63fb25
k-aff <u23543087@tuks.co.za>
2024-07-16 21:36:14 +0200

    index uP

--- FrontEnd/craftlink/src/index.js
+++ FrontEnd/craftlink/src/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter, createBrowserRouter,RouterProvider } from "react-router-dom";
+import Homepage from './Components/HomePage/homePage'
+import ServiceForm from './Components/Services/ServiceForm'
+import Navbar from './Components/NavBar/NavBar';
+import Layout from './Components/Layout';
+
+const router = createBrowserRouter([{
+  path: "/landingpage",
+  element: <Landing/>,
+
+  path:'/',
+  element: <Layout/>,
+  children:[
+    {
+      path: '/',
+      element: <Homepage/>
+    },
+    {
+path: '/services',
+element: <ServiceForm/>,
+    }
+  ]
+
+}]);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    
+    
+    <RouterProvider router={router}/>
+
   </React.StrictMode>
 );
-
