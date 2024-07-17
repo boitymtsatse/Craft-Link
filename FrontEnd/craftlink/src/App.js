8954ec3ca46c5c30f33a23691994e5c5e2029373
mainmee <diyasepaballo@gmail.com>
2024-07-17 12:59:16 +0200

    service page

--- FrontEnd/craftlink/src/App.js
+++ FrontEnd/craftlink/src/App.js
@@ -1,14 +1,44 @@
 
+// import React from 'react';
+// import Navbar from './Components/NavBar/NavBar';
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Navbar />
+      
+//     </div>
+    
+//   );
+// };
+
+// export default App
+//////////////////////////////////////
+
+// src/App.js
 import React from 'react';
+import './App.css';
 import Navbar from './Components/NavBar/NavBar';
+import Location from './Components/ServicePage/Location';
+import ServiceList from './Components/ServicePage/ServiceList';
 
 const App = () => {
-  return (
-    <div>
-      <Navbar />
-      
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar />
+            <header>
+                <h1>Service Provider</h1>
+                <nav>
+                    <ul>
+                        <li><a href="#location">Location</a></li>
+                        <li><a href="#services">Services</a></li>
+                    </ul>
+                </nav>
+            </header>
+            <Location />
+            <ServiceList />
+        </div>
+    );
 };
 
-export default App
+export default App;
