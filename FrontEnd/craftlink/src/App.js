db80a3a42a4981999989a880d2b377548d07b9c5
riri-bygit <rethabileb13@gmail.com>
2024-07-17 22:41:16 +0200

    .

--- FrontEnd/craftlink/src/App.js
+++ FrontEnd/craftlink/src/App.js
@@ -1,15 +1,15 @@
 
-// import React from 'react';
-// import Navbar from './Components/NavBar/NavBar';
-// import ServiceForm from './Components/Services/ServiceForm';
+import React from 'react';
+import Navbar from './Components/NavBar/NavBar';
+import ServiceForm from './Components/Services/ServiceForm';
 
-// const App = () => {
-//   return (
-//     <div>
-//       {/* <Navbar /> */}
-//       {/* <ServiceForm/> */}
-//     </div>
-//   );
-// };
+const App = () => {
+  return (
+    <div>
+      {/* <Navbar /> */}
+      {/* <ServiceForm/> */}
+    </div>
+  );
+};
 
-// export default App
+export default App
--- FrontEnd/craftlink/src/index.js
+++ FrontEnd/craftlink/src/index.js
@@ -1,38 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter, createBrowserRouter,RouterProvider } from "react-router-dom";
-import Homepage from './Components/HomePage/homePage'
-import ServiceForm from './Components/Services/ServiceForm'
-import Navbar from './Components/NavBar/NavBar';
-import Layout from './Components/Layout';
-
-const router = createBrowserRouter([{
-  path: "/landingpage",
-  element: <Landing/>,
-
-  path:'/',
-  element: <Layout/>,
-  children:[
-    {
-      path: '/',
-      element: <Homepage/>
-    },
-    {
-path: '/services',
-element: <ServiceForm/>,
-    }
-  ]
-
-}]);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Homepage from "./Components/HomePage/homePage";
+import ServiceForm from "./Components/Services/ServiceForm";
+import Navbar from "./Components/NavBar/NavBar";
+import Layout from "./Components/Layout";
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+   
+  
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    
-    <RouterProvider router={router}/>
-
+    <App />
   </React.StrictMode>
 );
-
