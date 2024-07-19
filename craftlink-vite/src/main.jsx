08ba38ff23d43c64bb58c8dabc88e8c97faf37b7
riri-bygit <rethabileb13@gmail.com>
2024-07-19 14:21:46 +0200

    craft-link

---
+++ craftlink-vite/src/main.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Adjust the path as necessary
+
+// Render the App component into the DOM element with the id 'root'
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
